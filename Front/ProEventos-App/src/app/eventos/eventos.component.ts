0c2abafd7d56bb702eef9cabbbbdba5f5e6dfd36
Luthianni Alves <luthgoran@gmail.com>
2021-04-27 21:30:23 -0300

    Seção 5: Angular, Interpolação, Diretiva e Binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
@@ -8,7 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  larguraImagem: number = 150;
+  margemImagem: Number = 2;
+  exibirImagem: boolean = true;
+  private _filtroLista: string = '';
+  eventosFiltrados: any;
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ?
+    this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase()
+      .indexOf(filtrarPor) === -1 ||
+      evento.local.toLocaleLowerCase()
+      .indexOf(filtrarPor) === -1
+    );
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,12 +42,20 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+
+  alterarImagem() {
+    this.exibirImagem = !this.exibirImagem;
+  }
+
   public getEventos( ): void {
-    this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+    this.http.get('https://localhost:5001/api/eventos')
+     .subscribe(
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     );
   }
-
 
 }
