8b3e9832b56ae1a89e92d5b6f704294305fd26f9
Luthianni Alves <57681298+Luthianni@users.noreply.github.com>
2021-05-02 20:38:11 -0300

    Revert "Seção 5: Angular, Interpolação, Diretiva e Binding"
    This reverts commit 0c2abafd7d56bb702eef9cabbbbdba5f5e6dfd36.

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -2,15 +2,11 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
-import { NavComponent } from './nav/nav.component';
-
-import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 
 @NgModule({
@@ -18,15 +14,12 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
     AppComponent,
       EventosComponent,
       PalestrantesComponent,
-      NavComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule,
-    CollapseModule.forRoot(),
-    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
@@ -9,32 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any = [];
-  larguraImagem: number = 150;
-  margemImagem: Number = 2;
-  exibirImagem: boolean = true;
-  private _filtroLista: string = '';
-  eventosFiltrados: any;
-
-  public get filtroLista(): string {
-    return this._filtroLista;
-  }
-
-  public set filtroLista(value: string) {
-    this._filtroLista = value;
-    this.eventosFiltrados = this.filtroLista ?
-    this.filtrarEventos(this.filtroLista) : this.eventos;
-  }
-
-  filtrarEventos(filtrarPor: string): any {
-    filtrarPor = filtrarPor.toLocaleLowerCase();
-    return this.eventos.filter(
-      evento => evento.tema.toLocaleLowerCase()
-      .indexOf(filtrarPor) === -1 ||
-      evento.local.toLocaleLowerCase()
-      .indexOf(filtrarPor) === -1
-    );
-  }
+  public eventos: any;
 
   constructor(private http: HttpClient) { }
 
@@ -42,20 +16,12 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
-
-  alterarImagem() {
-    this.exibirImagem = !this.exibirImagem;
-  }
-
   public getEventos( ): void {
-    this.http.get('https://localhost:5001/api/eventos')
-     .subscribe(
-      response => {
-        this.eventos = response;
-        this.eventosFiltrados = this.eventos;
-      },
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      response => this.eventos = response,
       error => console.log(error)
     );
   }
+
 
 }
